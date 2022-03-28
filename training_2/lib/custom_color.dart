d9bc17cf6be5e50fed2a0579c586e98b79d04567
daffa18 <80708687+daffa18@users.noreply.github.com>
2022-03-28 07:44:54 +0700

    Delete training_2 directory

--- training_2/lib/custom_color.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color blueCustom = const Color(0xff40739e);
--- training_2/lib/font_style.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:training_2/custom_color.dart';
-import 'custom_color.dart';
-
-TextStyle mainHeader =
-    TextStyle(fontSize: 25, color: blueCustom, fontFamily: 'Poppins');
